1400da1b2274476d4a41e26dfec38e490f0ee6f8
Pete Heard <peter_heard01@hotmail.com>
2018-01-06 10:02:21 +0000

    fixed the sign in component

--- auth/sign-in/sign-in-presenter.test.js
+++ auth/sign-in/sign-in-presenter.test.js
@@ -4,19 +4,24 @@ import FakeComponent from '../../test/fake-component'
 import StorageGateway from "../../common/storage-gateway";
 import FakeStore from "../../test/fake-store";
 import StubGenerator from "../../test/stub-generator";
+import AppComponentPresenter from "../../app/app-component.presenter";
 
 let fakeStore = null;
-let fakeComponent = null;
+let fakeAppComponent = null;
+let fakeSignInComponent = null;
 let signInPresenter = null;
+let appComponentPresenter = null;
 
 beforeEach(() => {
 	fakeStore = new FakeStore();
-	fakeComponent = new FakeComponent(fakeStore);
-	signInPresenter = new SignInPresenter(fakeComponent.dispatch);
+	fakeAppComponent = new FakeComponent(fakeStore);
+	fakeSignInComponent = new FakeComponent(fakeStore);
+	appComponentPresenter = new AppComponentPresenter(fakeAppComponent.dispatch);
+	signInPresenter = new SignInPresenter(fakeSignInComponent.dispatch);
 });
 
 it('should set initial viewmodel into component', async() => {
-	expect(fakeComponent.props.signInViewModel).toEqual({});
+	expect(fakeSignInComponent.props.signInViewModel).toEqual({});
 });
 
 it('should set intial state', async() => {
@@ -26,36 +31,36 @@ it('should set intial state', async() => {
 
 it('should load initial viewmodel', async() => {
 	signInPresenter.load();
-	expect(fakeComponent.props.signInViewModel.status).toBe('not-submitted');
-	expect(fakeComponent.props.signInViewModel.message).toBe('');
+	expect(fakeSignInComponent.props.signInViewModel.status).toBe('not-submitted');
+	expect(fakeSignInComponent.props.signInViewModel.message).toBe('');
 });
 
 it('successful sign-in', async() => {
 
-	fakeComponent.state = {userName:'blah', password:'blah_1'};
+	fakeSignInComponent.state = {userName:'blah', password:'blah_1'};
 	ApiGateway.prototype.save = StubGenerator.successfulLogin();
 	spyOn(ApiGateway.prototype,'save').and.callThrough();
 	spyOn(StorageGateway,'set');
 
-	await signInPresenter.signIn(fakeComponent);
+	await signInPresenter.signIn(fakeSignInComponent);
 
 	expect(ApiGateway.prototype.save).toHaveBeenCalledWith({userName:'blah', password:'blah_1'});
-	expect(fakeComponent.props.signInViewModel.status).toBe('submitted-success');
-	expect(fakeComponent.props.signInViewModel.message).toBe('user signIn successful');
+	expect(fakeSignInComponent.props.signInViewModel.status).toBe('submitted-success');
+	expect(fakeSignInComponent.props.signInViewModel.message).toBe('user signIn successful');
 	expect(StorageGateway.set).toHaveBeenCalledWith('userToken', '123');
 
 });
 
 it('un-successful sign-in', async() => {
 
-	fakeComponent.state = {userName:'blah', password:'blah_1'};
+	fakeSignInComponent.state = {userName:'blah', password:'blah_1'};
 	ApiGateway.prototype.save = StubGenerator.failedLogin();
 	spyOn(StorageGateway,'set');
 
-	await signInPresenter.signIn(fakeComponent);
+	await signInPresenter.signIn(fakeSignInComponent);
 
-	expect(fakeComponent.props.signInViewModel.status).toBe('submitted-failed');
-	expect(fakeComponent.props.signInViewModel.message).toBe('user signIn failed');
+	expect(fakeSignInComponent.props.signInViewModel.status).toBe('submitted-failed');
+	expect(fakeSignInComponent.props.signInViewModel.message).toBe('user signIn failed');
 	expect(StorageGateway.set).not.toHaveBeenCalled();
 
 });
