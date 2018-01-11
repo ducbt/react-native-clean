af6fb8395add888332f3739c7abb9b70b0e3b8de
Pete Heard <peter_heard01@hotmail.com>
2018-01-11 07:02:47 +0000

    fixed the sign in component

--- app/app-component.js
+++ app/app-component.js
@@ -5,32 +5,43 @@ import {
 import {StackNavigator, DrawerNavigator} from "react-navigation";
 import HomeComponent from "../home/home-component";
 import SignInComponent from "../auth/sign-in/sign-in-component";
+import AutoWire from "../common/auto-wire";
+import {connect} from "react-redux";
+import AppComponentPresenter from "./app-component.presenter";
 
 
+let topDrawer = DrawerNavigator({
+	Home: {screen: HomeComponent},
+	SignIn: {screen: SignInComponent}
+});
 
-export default class AppComponent  extends Component {
+//
+
+const AppNavigator = StackNavigator({
+	Drawer: {screen: topDrawer},
+}, {
+	navigationOptions: ({navigation}) => ({
+		headerLeft: <Button onPress={() => {
+			navigation.navigate('DrawerToggle')
+		}} title="Menu"/>
+	})
+});
+
+class AppComponent  extends Component {
+
+
+	componentWillMount() {
+		console.log('component mounting');
+	}
 
 	render() {
-
-		const items = {
-			Home: {screen: HomeComponent},
-			SignIn: {screen: SignInComponent}
-		};
-		const topDrawer = DrawerNavigator(items);
-
-		const AppNavigator = StackNavigator({
-			Drawer: {screen: topDrawer},
-		}, {
-			navigationOptions: ({navigation}) => ({
-				headerLeft: <Button onPress={() => {
-					navigation.navigate('DrawerToggle')
-				}} title="Menu"/>
-			})
-		});
-		
 		return (
 			<AppNavigator/>
 		);
 	}
 }
+
+export default connect((state) => {
+	return state;
+}, (dispatch) => (AutoWire.wire(AppComponentPresenter, dispatch,[])))(AppComponent);
 
